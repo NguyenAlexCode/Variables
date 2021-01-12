4183b43cd4dee001e5e904236b83079d9134f437
Nguyen Alex <110190@eesd.org>
2021-01-12 00:31:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Variables: containers, hold a value
+# Data Types/ Variables Types: 
+# String (str) : combination of characters (letters, numbers, or symbols, must use quotation marks)
+# Integer (int) : whole numbers
+# Character (char) : single leter, number or symbol
+# FLoat : numbers with decimals
+# Bollean (bool) : True/ False
+
+# formula:
+# variableName = value
+# Rules for variable names
+# 1. always start with a letter
+# 2. can only contain numbers and letters
+# 3. if more thatn one word is used, use camelCase (first word is lower case and every word after that is uppercase)
+
+firstName = "Alex" #string type
+
+favBook = "Harry Potter and the Deathly Hallows"
+favumber = 0
+isHungry = False
+favChar = '$'
